7bb784c578d1e240fccf12ae047fdd782cd6a45c
Kevin Cotrina <kcotrinma92@gmail.com>
2021-04-29 15:59:05 -0500

    Add book reducer

---
+++ src/reducers/book.js
@@ -0,0 +1,27 @@
+import { CREATE_BOOK, DELETE_BOOK } from '../actions/types';
+
+const INITIAL_STATE = [
+	{
+		id: 1,
+		name: 'Harry Potter',
+		category: 'Sci-fi',
+	},
+	{
+		id: 2,
+		name: 'Star Trek',
+		category: 'Sci-fi',
+	},
+];
+
+const bookReducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case CREATE_BOOK:
+			return state;
+		case DELETE_BOOK:
+			return state;
+		default:
+			return state;
+	}
+};
+
+export default bookReducer;
