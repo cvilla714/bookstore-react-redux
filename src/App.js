1e25d5946c1abbce404be988aa92c2b48da166ac
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-29 18:42:37 -0400

    added boostrap library and imported provider and store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import BooksList from './components/BooksList';
+import BooksForm from './components/Booksform';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BooksList />
+        <BooksForm />
+      </div>
+    </Provider>
   );
 }
 
