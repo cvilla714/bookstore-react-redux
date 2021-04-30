209d5ff7a5998eb1d552e547a288c683e0f85f11
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-30 10:25:31 -0400

    added the List component and use map to iterate the list of books and display it

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -2,10 +2,9 @@ import { connect } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'react-bootstrap';
+import List from './List';
 
 const BooksList = ({ books }) => {
-  console.log(books);
-
   return (
     <>
       <Table striped bordered hover size="sm">
@@ -17,48 +16,20 @@ const BooksList = ({ books }) => {
           </tr>
         </thead>
         <tbody>
-          {/* {books.map((book) => (
-            <tr key={book.id}>
-              <td>{book.id}</td>
-              <td>{book.name}</td>
-              <td>{book.category}</td>
-            </tr>
-          ))} */}
-          <tr>
-            <td>1</td>
-            <td>Harry Potter</td>
-            <td>Sci-fi</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Star trek</td>
-            <td>Sci-fi</td>
-          </tr>
+          {books.book.map((book) => (
+            <List id={book.id} key={book.id} name={book.name} category={book.category} />
+          ))}
         </tbody>
       </Table>
     </>
   );
 };
 
-// BooksList.propTypes = {
-//   books: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       name: PropTypes.string.isRequired,
-//       id: PropTypes.number.isRequired,
-//       category: PropTypes.string.isRequired,
-//     })
-//   ),
-// };
-
 BooksList.propTypes = {
   books: PropTypes.objectOf(Array).isRequired,
 };
 
-// BooksList.propTypes = {
-//   books: PropTypes.objectOf(PropTypes.any).isRequired,
-// };
-
 const mapStateToProps = (state) => ({
   books: state,
 });
-export default connect(mapStateToProps, null)(BooksList);
+export default connect(mapStateToProps)(BooksList);
