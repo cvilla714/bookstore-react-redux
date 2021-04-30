7ff239d9055ab6a1bce92e2bdc880436dfda3e10
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-30 12:10:02 -0400

    deleted return and instead showing the table

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -4,26 +4,24 @@ import PropTypes from 'prop-types';
 import { Table } from 'react-bootstrap';
 import List from './List';
 
-const BooksList = ({ books }) => {
-  return (
-    <>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.book.map((book) => (
-            <List id={book.id} key={book.id} name={book.name} category={book.category} />
-          ))}
-        </tbody>
-      </Table>
-    </>
-  );
-};
+const BooksList = ({ books }) => (
+  <>
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.book.map((book) => (
+          <List id={book.id} key={book.id} name={book.name} category={book.category} />
+        ))}
+      </tbody>
+    </Table>
+  </>
+);
 
 BooksList.propTypes = {
   books: PropTypes.objectOf(Array).isRequired,
