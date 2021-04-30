9434d8abbd98fb4117a0b5ced06efdc81482dde4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-30 19:58:10 -0400

    imported icon, actions and created the function to delete the book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { RiDeleteBinFill } from 'react-icons/ri';
+import { connect } from 'react-redux';
+import { deleteBookAction } from '../actions/index';
 
-const List = ({ id, name, category }) => (
-  <tr>
-    <td>{id}</td>
-    <td>{name}</td>
-    <td>{category}</td>
-  </tr>
-);
+const List = ({
+  id, name, category, deleteBook,
+}) => {
+  const onDelete = () => {
+    deleteBook(id);
+  };
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{name}</td>
+      <td>{category}</td>
+      <td>
+        {' '}
+        <a href="#!" onClick={onDelete}>
+          <RiDeleteBinFill />
+        </a>
+      </td>
+    </tr>
+  );
+};
 
 List.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
-export default List;
+const mapDispatchToProps = (dispatch) => {
+  const deleteBook = (bookid) => dispatch(deleteBookAction(bookid));
+
+  return { deleteBook };
+};
+
+export default connect(null, mapDispatchToProps)(List);
