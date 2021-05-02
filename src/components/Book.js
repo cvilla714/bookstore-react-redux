53039a0dacfc4320c42c25d0fb91725fcb7af329
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-01 21:11:37 -0400

    extracted the deleted function and added it to the Booklist,pased the id to the onDelete function and pass the function as a prop

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,41 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { RiDeleteBinFill } from 'react-icons/ri';
-import { connect } from 'react-redux';
-import { deleteBookAction } from '../actions/index';
 
 const List = ({
-  id, name, category, deleteBook,
-}) => {
-  const onDelete = () => {
-    deleteBook(id);
-  };
-  return (
-    <tr>
-      <td>{id}</td>
-      <td>{name}</td>
-      <td>{category}</td>
-      <td>
-        {' '}
-        <a href="#!" onClick={onDelete}>
-          <RiDeleteBinFill />
-        </a>
-      </td>
-    </tr>
-  );
-};
+  id, name, category, onDelete,
+}) => (
+  <tr>
+    <td>{id}</td>
+    <td>{name}</td>
+    <td>{category}</td>
+    <td>
+      {' '}
+      <a href="#!" onClick={() => onDelete(id)}>
+        <RiDeleteBinFill />
+      </a>
+    </td>
+  </tr>
+);
 
 List.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
-  deleteBook: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
-const mapDispatchToProps = (dispatch) => {
-  const deleteBook = (bookid) => dispatch(deleteBookAction(bookid));
-
-  return { deleteBook };
-};
-
-export default connect(null, mapDispatchToProps)(List);
+export default List;
