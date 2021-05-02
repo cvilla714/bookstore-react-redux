b3943c06b2fdc6512b9aeb7062543c225a7f4c33
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-01 21:12:39 -0400

    added the action to delted the book and passed a new function to handle delete book, also added the proptype

--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -3,31 +3,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'react-bootstrap';
 import Book from '../components/Book';
+import { deleteBookAction } from '../actions/index';
 
-const BooksList = ({ books }) => (
-  <>
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.book.map((book) => (
-          <Book id={book.id} key={book.id} name={book.name} category={book.category} />
-        ))}
-      </tbody>
-    </Table>
-  </>
-);
+const BooksList = ({ books, deleteBookAction }) => {
+  const handleDelete = (id) => {
+    deleteBookAction(id);
+  };
+  return (
+    <>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.book.map((book) => (
+            <Book
+              id={book.id}
+              key={book.id}
+              name={book.name}
+              category={book.category}
+              onDelete={handleDelete}
+            />
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
 
 BooksList.propTypes = {
   books: PropTypes.objectOf(Array).isRequired,
+  deleteBookAction: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   books: state,
 });
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, { deleteBookAction })(BooksList);
