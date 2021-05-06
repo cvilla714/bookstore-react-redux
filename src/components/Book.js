cc25da76d5c697f1dfd15b818e5c58c6b78d014a
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-06 00:53:46 -0500

    Refactor Book component styles

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,35 @@
 /* eslint-disable object-curly-newline */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const List = ({ id, name, author, category, onDelete }) => (
-  <div className="card z-depth-2 row .card-fixer">
+  <div className="card z-depth-2 row .card-fixer valign-wrapper">
     <div className="col s7">
       <div className="card-info">
         <h6 className="header grey-text text-lighten-1">{category}</h6>
         <h5 className="header grey-text text-darken-4">{name}</h5>
-        <p className="grey-text text-lighten-1">{author}</p>
+        <p className="blue-text text-lighten-3">{author}</p>
       </div>
+
       <div className="buttons-container">
-        <a href="!#" className="btn-flat">
+        <a href="!#" className="btn-flat blue-text text-lighten-3">
           Comments
         </a>
-        <a href="#!" onClick={() => onDelete(id)} className="btn-flat">
+        <a
+          href="#!"
+          onClick={() => onDelete(id)}
+          className="btn-flat blue-text text-lighten-3"
+        >
           Remove
         </a>
-        <a href="!#" className="btn-flat">
+        <a href="!#" className="btn-flat blue-text text-lighten-3">
           Edit
         </a>
       </div>
     </div>
     <div className="col s5 row">
-      <div className="col s6">15%</div>
+      <div className="col s6">progressbar</div>
       <div className="col s6">
         <p>CURRENT CHAPTER</p>
         <p>
