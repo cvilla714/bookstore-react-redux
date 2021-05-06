dec2f06045b69736b869fbc7f86e9b7c583a2143
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-05 19:37:04 -0500

    Fix navbar overlaping

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,22 +3,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const List = ({ id, name, author, category, onDelete }) => (
-  <div className="card z-depth-2 row">
+  <div className="card z-depth-2 row .card-fixer">
     <div className="col s7">
       <div className="card-info">
         <h6 className="header grey-text text-lighten-1">{category}</h6>
         <h5 className="header grey-text text-darken-4">{name}</h5>
         <p className="grey-text text-lighten-1">{author}</p>
       </div>
-      <a href="!#" className="btn-flat">
-        Comments
-      </a>
-      <a href="#!" onClick={() => onDelete(id)} className="btn-flat">
-        Remove
-      </a>
-      <a href="!#" className="btn-flat">
-        Edit
-      </a>
+      <div className="buttons-container">
+        <a href="!#" className="btn-flat">
+          Comments
+        </a>
+        <a href="#!" onClick={() => onDelete(id)} className="btn-flat">
+          Remove
+        </a>
+        <a href="!#" className="btn-flat">
+          Edit
+        </a>
+      </div>
     </div>
     <div className="col s5 row">
       <div className="col s6">15%</div>
